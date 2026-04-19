ee594055d59333fbd56daea93ace92bbad2805d4
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2026-04-19 19:29:41 -0300

    .

--- relacao_simples.pl
+++ relacao_simples.pl
@@ -1,16 +1,27 @@
+:- encoding(utf8). % para poder usar o acento
+
 % FATOS
-professor(francisco).			% francisco eh um professor
-professor(alexandre).			% alexandre eh um professor
-aluno(gustavo).					% gustavo eh um aluno
-aluno(cristian).				% cristian eh um aluno
-aluno(pedro).					% pedro eh um aluno
-disciplina(edl, francisco, 1).	% edl eh uma disciplina dada por francisco na turma 1
-disciplina(lp2, alexandre, 2).	% lp2 eh uma disciplina dada por alexandre na turma 2
-matriculado(gustavo, edl, 1).	% gustavo esta matriculado em edl turma 1
-matriculado(cristian, edl, 1).	% cristian esta matriculado em edl turma 1
-matriculado(pedro, lp2, 2).		% pedro esta matriculado em lp2 turma 2
+pai(pablo_escobar, sebastian).
+pai(pablo_escobar, manuela).
+pai(abel, pablo_escobar).
+
+mae(tata, sebastian).
+mae(tata, manuela).
+mae(hermilda, pablo_escobar).
 
 % REGRA
-tem_aula_com(Aluno, Professor) :- 				% Regra para checar se um aluno tem aula com tal professor. O que acontece quando:
-    matriculado(Aluno, Disciplina, Turma),		% 1 - o aluno esta matricula em Discplina na turma Turma; e
-    disciplina(Disciplina, Professor, Turma).	% 2 - professor dá aula da Disciplina na turma Turma
+avô(X, Y) :- pai(X, Z), (pai(Z, Y); mae(Z, Y)).
+avó(X, Y) :- mae(X, Z), (pai(Z, Y); mae(Z, Y)).
+
+% OU
+% avô(X, Y) :- pai(X, Z), pai(Z, Y).
+% avô(X, Y) :- pai(X, Z), mae(Z, Y).
+% avó(X, Y) :- mae(X, Z), pai(Z, Y).
+% avó(X, Y) :- mae(X, Z), mae(Z, Y).
+
+% OU
+% progenitor(X, Y) :- pai(X, Y).
+% progenitor(X, Y) :- mae(X, Y).
+%
+% avô(X, Y) :- pai(X, Z), progenitor(X, Y).
+% avó(X, Y) :- mae(X, Z), progenitor(X, Y).
--- relacao_simples2.pl
+++ relacao_simples2.pl
@@ -1,27 +1,16 @@
-:- encoding(utf8). % para poder usar o acento
-
 % FATOS
-pai(pablo_escobar, sebastian).
-pai(pablo_escobar, manuela).
-pai(abel, pablo_escobar).
-
-mae(tata, sebastian).
-mae(tata, manuela).
-mae(hermilda, pablo_escobar).
+professor(francisco).			% francisco eh um professor
+professor(alexandre).			% alexandre eh um professor
+aluno(gustavo).					% gustavo eh um aluno
+aluno(cristian).				% cristian eh um aluno
+aluno(pedro).					% pedro eh um aluno
+disciplina(edl, francisco, 1).	% edl eh uma disciplina dada por francisco na turma 1
+disciplina(lp2, alexandre, 2).	% lp2 eh uma disciplina dada por alexandre na turma 2
+matriculado(gustavo, edl, 1).	% gustavo esta matriculado em edl turma 1
+matriculado(cristian, edl, 1).	% cristian esta matriculado em edl turma 1
+matriculado(pedro, lp2, 2).		% pedro esta matriculado em lp2 turma 2
 
 % REGRA
-avô(X, Y) :- pai(X, Z), (pai(Z, Y); mae(Z, Y)).
-avó(X, Y) :- mae(X, Z), (pai(Z, Y); mae(Z, Y)).
-
-% OU
-% avô(X, Y) :- pai(X, Z), pai(Z, Y).
-% avô(X, Y) :- pai(X, Z), mae(Z, Y).
-% avó(X, Y) :- mae(X, Z), pai(Z, Y).
-% avó(X, Y) :- mae(X, Z), mae(Z, Y).
-
-% OU
-% progenitor(X, Y) :- pai(X, Y).
-% progenitor(X, Y) :- mae(X, Y).
-%
-% avô(X, Y) :- pai(X, Z), progenitor(X, Y).
-% avó(X, Y) :- mae(X, Z), progenitor(X, Y).
+tem_aula_com(Aluno, Professor) :- 				% Regra para checar se um aluno tem aula com tal professor. O que acontece quando:
+    matriculado(Aluno, Disciplina, Turma),		% 1 - o aluno esta matricula em Discplina na turma Turma; e
+    disciplina(Disciplina, Professor, Turma).	% 2 - professor dá aula da Disciplina na turma Turma
