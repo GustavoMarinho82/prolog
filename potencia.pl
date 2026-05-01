b9b8e74c1b7df9d52fcd0f3b8110c5f77a299909
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2026-05-01 19:18:14 -0300

    Adicionado casos para potencias com expoentes negativos

--- potencia.pl
+++ potencia.pl
@@ -6,6 +6,13 @@ potencia(_, 0, 1) :- !. % Caso o expoente seja 0, essa declaracao da regra eh ex
 
 % B = base; E = expoente; R = resultado
 potencia(B, E, R) :-
+	E < 0,					% Verifica se E eh negativo. Se nao for, o Prolog tenta executar a proxima declaracao de potencia via backtracking automatico, porque essa ja falhou
+	!,						% Se ele chegar nessa clausula, o expoente eh menor que 0 e a recursao vai funcionar. O ! impede que o Prolog tente a outra declaracao de potencia apos essa finalizar
+	B2 is 1 / B,			% B2 = 1 / B
+	E2 is -E,				% E2 = -E
+	potencia(B2, E2, R).	% Faz a recursao com potencia(1 / B, -E, R)
+	
+potencia(B, E, R) :-
 	E2 is E - 1,			% E2 = E-1
 	potencia(B, E2, R2),	% Faz a recursao de potencia com o novo expoente e salva o resultado em R2
 	R is B * R2.			% R = B * B^(E-1)
--- potencia_otimizada.pl
+++ potencia_otimizada.pl
@@ -9,9 +9,16 @@ potencia(_, 0, 1) :- !.	% Caso o expoente seja 0, essa declaracao da regra eh ex
 
 % B = base; E = expoente; R = resultado
 potencia(B, E, R) :-
+	E < 0,					% Verifica se E eh negativo. Se nao for, o Prolog tenta executar a proxima declaracao de potencia via backtracking automatico, porque essa ja falhou
+	!,						% Se ele chegar nessa clausula, o expoente eh menor que 0 e a recursao vai funcionar. O ! impede que o Prolog tente a outra declaracao de potencia apos essa finalizar
+	B2 is 1 / B,			% B2 = 1 / B
+	E2 is -E,				% E2 = -E
+	potencia(B2, E2, R).	% Faz a recursao com potencia(1 / B, -E, R)
+
+potencia(B, E, R) :-
 	E mod 2 =:= 0,			% Verifica se o expoente eh par. Se nao for, o Prolog tenta executar a proxima declaracao de potencia via backtracking automatico, porque essa ja falhou
 	!,						% Se ele chegar nessa clausula, o expoente eh par e a recursao vai funcionar. O ! impede que o Prolog tente a outra declaracao de potencia apos essa finalizar
-	E2 is E // 2,			% E2 = E / 2
+	E2 is E // 2,			% E2 = E // 2 (se nao fosse divisao inteira retornaria um float)
 	potencia(B, E2, R2),	% Faz a recursao de potencia com o novo expoente e salva o resultado em R2
 	R is R2 * R2.			% R = B^(E/2) * B^(E/2)
 
